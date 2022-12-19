ac049a4c10d0275cf65e13d9651559c6477f91e6
AugustoS-Silva <106820674+AugustoS-Silva@users.noreply.github.com>
2022-12-19 18:59:34 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+func separar(arr []int, menor, maior int) int {
+	//Seleciona o ultimo indice do array
+	ultimoValor := arr[maior]
+	for j := menor; j < maior; j++ {
+		//Verifica se o valor dentro do array é menor que o ultimo indice, caso sim ele inverte as posições do valor encontrado com o menor valor
+		if arr[j] < ultimoValor {
+			arr[j], arr[menor] = arr[menor], arr[j]
+			menor++
+		}
+	}
+	//Muda a posição dos menores valores para antes dos maiores valores
+	arr[menor], arr[maior] = arr[maior], arr[menor]
+	return menor
+}
+
+func ordenar(arr []int, menor, maior int) {
+	//Checa até que o menor valor seja maior
+	if menor > maior {
+		return
+	}
+	//Separa o array
+	pivo := separar(arr, menor, maior)
+	//Ordena os valores a esquerda do pivo
+	ordenar(arr, menor, pivo-1)
+	//Ordena os valores a direita do pivo
+	ordenar(arr, pivo+1, maior)
+}
+
+// Declara o array e realiza a chamada da função para ordenar
+func main() {
+	arr := []int{22, 7, 9, 12, 8, 36, 45, 92, 54, 82, 63, 15, 4, 3}
+	ordenar(arr, 0, len(arr)-1)
+	fmt.Println(arr)
+}
